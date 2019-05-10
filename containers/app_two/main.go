ed8421cc2b4fc0d98065236b000428237184bda2
Graham Plata <graham.plata@gmail.com>
2019-05-10 16:55:18 -0400

    Updates (#1)
    * update name of app
    
    * app_one
    
    * working go cron app
    
    * demo cron workaround
    
    * cleanup

---
+++ containers/app_two/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/push"
+)
+
+func PushExampleData() {
+	exampleCron := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "cron_completion_timestamp",
+		Help: "A Phoney Baloney generated timestamp",
+	})
+	exampleCron.SetToCurrentTime()
+	if err := push.New(os.Getenv("DOCKER_NETWORK"), "cron").
+		Collector(exampleCron).
+		Push(); err != nil {
+		fmt.Println("Could not push to Pushgateway:", err)
+	}
+}
+
+func main() {
+	PushExampleData()
+}
